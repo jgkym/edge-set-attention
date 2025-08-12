8904045de7f8b8378617e07f81adb193069f8048
jgkym <jingi.data@gmail.com>
2025-08-13 00:25:36 +0900

    feat: Add prefix argument

--- esa/metrics.py
+++ esa/metrics.py
@@ -5,12 +5,12 @@ from torchmetrics.functional import (
 )
 
 
-def compute_metrics(preds, target):
+def compute_metrics(preds, target, prefix: str | None = None):
     nrmse = normalized_root_mean_squared_error(preds, target, normalization="range")
     r2 = pearson_corrcoef(preds, target) ** 2
     final_score = 0.4 * (1 - torch.clamp(nrmse, max=1.0)) + 0.6 * r2
     return {
-        "score": final_score,
-        "nrmse": nrmse,
-        "r2": r2,
+        f"{prefix}score": final_score,
+        f"{prefix}nrmse": nrmse,
+        f"{prefix}r2": r2,
     }
